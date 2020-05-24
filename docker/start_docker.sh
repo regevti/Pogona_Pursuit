ba0df83edc2825eaf684057574be7beee5a63f5d
Regev <regevti@gmail.com>
2020-05-24 17:41:42 +0300

    docker

---
+++ docker/start_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+container_name=pogona
+docker stop ${container_name} && docker rm ${container_name}
+
+docker run -itd --gpus all --name ${container_name} -p 2350:8888 --restart always --privileged \
+      -v /media/sil2/regev/:/app/ \
+      -v /etc/udev/:/etc/udev/ \
+      -v /dev/bus/usb/:/dev/bus/usb/ \
+      pogona_pursuit:0.1
