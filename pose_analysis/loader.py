b176181e4f24a45e609bb5e7fc382f0147c520f8
Regev Eyal <regevti@gmail.com>
2020-12-05 12:55:48 +0200

    pose

--- pose_analysis/loader.py
+++ pose_analysis/loader.py
@@ -119,6 +119,9 @@ def closest_index(series, x):
 def get_experiments(*args, **kwargs):
     """Get experiment using explore"""
     df = ExperimentAnalyzer(*args, **kwargs).get_experiments()
+    if not df:
+        print('No experiment found')
+        return
     loaders = []
     for experiment, trial in df.index:
         try:
