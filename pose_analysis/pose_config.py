6d4cf871df36313713a9a4562ff1617c0a60648e
Regev Eyal <regevti@gmail.com>
2021-04-10 18:20:25 +0300

    pose

--- pose_analysis/pose_config.py
+++ pose_analysis/pose_config.py
@@ -18,9 +18,9 @@ CAMERAS = {
 SCREEN_BOUNDARIES = {'x': (0, 1850), 'y': (0, 800)}
 
 DLC_PROJECTS_PATH = MAIN_PATH + '/pose_estimation/deeplabcut/projects'
-DLC_PATH = DLC_PROJECTS_PATH + '/pogona_pursuit_realtime'
+DLC_PATH = DLC_PROJECTS_PATH + '/dlc_realtime'
 DLC_CONFIG_FILE = DLC_PATH + '/config.yaml'
-ITERATION = 3
-EXPORTED_MODEL_PATH = DLC_PATH + f'/exported-models/DLC_pogona_pursuit_resnet_50_iteration-{ITERATION}_shuffle-1'
+ITERATION = 4
+EXPORTED_MODEL_PATH = DLC_PATH + f'/exported-models/DLC_realtime_dlc_resnet_50_iteration-{ITERATION}_shuffle-1'
 PROBABILITY_THRESH = 0.85
 BODY_PARTS = ['nose', 'left_ear', 'right_ear']
