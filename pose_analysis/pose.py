a885e55afea8db21475669ebaddc3efbf3d8acca
Regev Eyal <regevti@gmail.com>
2021-04-10 18:15:34 +0300

    pose

--- pose_analysis/pose.py
+++ pose_analysis/pose.py
@@ -43,59 +43,67 @@ class PoseAnalyzer:
         model_name = model_path.parts[5] + f'_iteration{iteration}'
         return DLCLive(model_path.as_posix(), processor=Processor()), model_name, model_path
 
-    def run_pose(self, selected_frames=None, is_save_frames=False, load_only=False) -> pd.DataFrame:
+    def run_pose(self, load_only=False) -> pd.DataFrame:
         """
         Run Pose Estimation
-        :param selected_frames: List of frames IDs needed to be analyzed (ignore the rest)
-        :param is_save_frames: True for saving frames in self.saved_frames
+        :param load_only: don't run inference, only try to load the csv pose file.
         :return: Dataframe with frames as index and body parts as columns
         """
-        if selected_frames:
-            selected_frames.sort()
-        if not selected_frames:
-            if self.output_video_path.exists():
-                Compressor(self.output_video_path).compress()
-            if self.pose_df is not None:
-                return self.pose_df
-            elif load_only:
-                raise Exception(f'cannot find video for {self.loader.day_dir}, trial{self.loader.trial_id}')
-
-        cap = cv2.VideoCapture(self.video_path.as_posix())
         res = []
-        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        print(f'start pose estimation for {self.loader.day_dir} trial{self.loader.trial_id}')
-        for frame_id in tqdm(range(num_frames)):
-            ret, frame = cap.read()
-            if selected_frames and frame_id not in selected_frames:
-                continue
-            if ret:
-                if not self.is_dlc_live_initiated:
-                    self.dlc_live.init_inference(frame)
-                    self.is_dlc_live_initiated = True
-                # Initialize video writer (only if no specific frames provided)
-                if not selected_frames and self.video_out is None:
-                    fps = cap.get(cv2.CAP_PROP_FPS)
-                    h, w = frame.shape[:2]
-                    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-                    print(f'Saving analyzed video to: {self.output_video_path}')
-                    self.video_out = cv2.VideoWriter(self.output_video_path.as_posix(), fourcc, fps, (w, h))
+        df = self.check_pose(load_only)
+        if df is not None:
+            return df
+        try:
+            cap = cv2.VideoCapture(self.video_path.as_posix())
+            num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            print(f'start pose estimation for {self.loader.day_dir} trial{self.loader.trial_id}')
+            for frame_id in tqdm(range(num_frames)):
+                ret, frame = cap.read()
+                if ret:
+                    self.init_dlc_live(frame)
+                    self.init_video_writer(cap, frame)
+                    pred = self.dlc_live.get_pose(frame)
+                    pred_df = self.create_pred_df(pred, frame_id)
+                    res.append(pred_df)
+                    self.write_frame(frame, frame_id, pred_df)
+                else:
+                    break
 
-                if is_save_frames:
-                    self.saved_frames[frame_id] = frame
-                    
-                pred = self.dlc_live.get_pose(frame)
-                pred_df = self.create_pred_df(pred, frame_id)
-                res.append(pred_df)
-                self.write_frame(frame, frame_id, pred_df)
-            if not ret or (selected_frames and frame_id > selected_frames[-1]):
-                break
+            self.video_out.release()
+            self.video_out = None
+            Compressor(self.output_video_path).compress()
+        except Exception as exc:
+            print(f'Error in run_pose: {exc}')
+        finally:
+            return self.save_csv(res)
 
-        self.video_out.release()
-        self.video_out = None
-        Compressor(self.output_video_path).compress()
+    def check_pose(self, load_only):
+        if self.output_video_path.exists():
+            Compressor(self.output_video_path).compress()
+        if self.pose_df is not None:
+            return self.pose_df
+        elif load_only:
+            raise Exception(f'cannot find video for {self.loader.day_dir}, trial{self.loader.trial_id}')
+
+    def init_dlc_live(self, frame):
+        if not self.is_dlc_live_initiated:
+            self.dlc_live.init_inference(frame)
+            self.is_dlc_live_initiated = True
+
+    def init_video_writer(self, cap, frame):
+        """Initialize video writer (only if no specific frames provided)"""
+        if self.video_out is None:
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            h, w = frame.shape[:2]
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            print(f'Saving analyzed video to: {self.output_video_path}')
+            self.video_out = cv2.VideoWriter(self.output_video_path.as_posix(), fourcc, fps, (w, h))
+
+    def save_csv(self, res):
         if res:
             df = pd.concat(res)
-            df.to_csv(self.output_video_path.parent / (self.output_video_path.stem + '.csv'))
+            csv_path = self.output_video_path.parent / (self.output_video_path.stem + '.csv')
+            df.to_csv(csv_path)
             return df
 
     @property
